9160a539a4271e5f4e164ee031aeeca3fb94f783
Louis Brutton <louisbrutton@gmail.com>
2015-04-21 15:24:49 +0200

    create rough draft for dashboard

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,5 +2,10 @@ class GamesController < ApplicationController
 	def index
 		#@games = Game.all
 		@tasks_grid = initialize_grid(Game)
+		@total_games = Game.count
+		@total_games_without_promo = Game.where(english_promo:"empty").count
+		@total_games_without_square_banners = Game.where(square_banner:"empty").count
+		@total_games_without_flat_banners = Game.where(flat_banner:"empty").count
+		@total_games_without_video = Game.where(english_vid:"empty").count
 	end
 end
