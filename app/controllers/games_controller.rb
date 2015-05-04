22557e73b4237f441f9cdaf91e694d73ef017896
Louis Brutton <louisbrutton@gmail.com>
2015-05-04 17:55:12 +0200

    clean up buttons, split rake task into one for dev, one for production, to avoid over using the proxy

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,7 +3,7 @@ class GamesController < ApplicationController
 	def index
 		#@games = Game.all
 		@tasks_grid = initialize_grid(Game)
-		@total_games = Game.count
+		@total_games = Game.where(in_db: true).count
 		@total_games_without_promo = Game.where(english_promo:"empty").count
 		@total_games_without_square_banners = Game.where(square_banner:"empty").count
 		@total_games_without_flat_banners = Game.where(flat_banner:"empty").count
